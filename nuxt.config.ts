20c66a04a1d825cd155aafa6567b9b9e70820484
miftahpj <142276212+miftahpj@users.noreply.github.com>
2026-08-26 09:39:42 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,12 @@ export default defineNuxtConfig({
         },
         { property: 'og:type', content: 'website' }
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
+      ]
     }
   },
 
